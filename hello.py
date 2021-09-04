ff0306cc5072c261f1101715e97f074b6b2a33ea
IvanPozd <johndillon28cb@gmail.com>
2021-09-04 18:20:11 +0300

    Создание репозитория Learn Python

---
+++ hello.py
@@ -0,0 +1,10 @@
+name = "Ivan"
+
+print("Hello World!")
+print("Hello,", name)
+print("Привет, Программист!")
+
+print()
+
+print("2 + 2 =", 2 + 2)
+print("10 / 3 =", 10 / 3)
---
+++ simple.py
@@ -0,0 +1,26 @@
+a = 2
+b = 0.5
+print(a + b)
+
+print()
+
+name = "Ivan"
+print(f"Привет, {name}")
+
+print()
+# Числа
+v = int(input("Введите число от 1 до 10: "))
+print(f"{v} + 10 =", v + 10)
+
+print()
+# Строки
+name = input("Введите Ваше имя: ").upper()
+print(f"Привет, {name}! Как дела?")
+
+print()
+# Приведение типов
+print(float('1'))
+print(bool(1))
+print(int(2.5))
+print(bool(''))
+print(bool(0))
